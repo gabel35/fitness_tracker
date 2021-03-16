d0a953ac1bedbc85c7a3baa58046eac16f08720c
gabel35 <gabrieljose3135@gmail.com>
2021-03-16 15:14:49 -0400

    adjusted server

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{ 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true 
+});
 
-app.use(require("./routes/html-routes.js"));
-app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-route.js"));
+app.use(require("./routes/api-route.js"));
 
 
 // Start the server
