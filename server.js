d936cdbeb1d0367981aa3b18b46e3c00c6755efe
gabel35 <gabrieljose3135@gmail.com>
2021-03-16 23:48:47 -0400

    push to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
